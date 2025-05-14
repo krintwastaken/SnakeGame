c65c23c2b9077c44e7fb87047a9b596f1fbe2e3d
krintwastaken <gamerofthe21thcentury@gmail.com>
2025-05-15 06:07:05 +0700

    Update authController.js

--- authController.js
+++ authController.js
@@ -1,5 +1,5 @@
-const User = require('./models/user')
-const Role = require('./models/role')
+const User = require('./models/User')
+const Role = require('./models/Role')
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const {validationResult} = require('express-validator')
