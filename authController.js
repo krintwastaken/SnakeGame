0aa221f34ed00dc25d8984475901df86b49557a1
krintwastaken <gamerofthe21thcentury@gmail.com>
2025-05-15 12:38:15 +0700

    Update authController.js

--- authController.js
+++ authController.js
@@ -77,7 +77,14 @@ class authController {
             const { password, email } = req.body;
     
             const user = await User.findOne({ email });
-            
+                        if (!user) {
+                return res.status(400).json({ message: `Пользователь с email: ${email} не найден` });
+            }
+    
+            const validPassword = bcrypt.compareSync(password, user.password);
+            if (!validPassword) {
+                return res.status(400).json({ message: `Введен неверный пароль` });
+            }
             const token = generateAccessToken(user._id, user.roles);
             return res.json({ token, message: "ok" });
         } catch (err) {
